080d74d973ec7f51b954fdf5829837a12db31023
Mateus Bauer <bauer.mmateus@gmail.com>
2022-12-22 23:32:35 -0300

    navegation implemented

--- app/src/main/java/com/mbs/workoutplanner/MainActivity.kt
+++ app/src/main/java/com/mbs/workoutplanner/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mbs.workoutplanner
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.mbs.workoutplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,19 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: WorkoutViewModel by viewModels {
         WorkoutViewModel.Factory(WorkoutRepository)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.workoutButton.setOnClickListener {
+            findNavController(R.id.fragmentContainerView).navigate(R.id.action_global_workoutFragment)
+        }
+        binding.calculateButton.setOnClickListener {
+            findNavController(R.id.fragmentContainerView).navigate(R.id.action_global_calculateFragment)
+        }
+        binding.profileButton.setOnClickListener {
+            findNavController(R.id.fragmentContainerView).navigate(R.id.action_global_profileFragment)
+        }
     }
 }
