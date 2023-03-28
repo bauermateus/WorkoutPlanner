21bfc946cb1770403fa953d605324c38c1e5d41a
bauermateus <bauer.mmateus@gmail.com>
2023-03-27 22:25:45 -0300

    login-fail

--- app/src/main/java/com/mbs/workoutplanner/view/MainActivity.kt
+++ app/src/main/java/com/mbs/workoutplanner/view/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mbs.workoutplanner.view
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -27,9 +30,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val viewModel: MainViewModel by viewModels()
     private lateinit var auth: FirebaseAuth
-    private var user: FirebaseUser? = null
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -47,45 +50,42 @@ class MainActivity : AppCompatActivity() {
             window.navigationBarDividerColor = ContextCompat.getColor(this, R.color.dark_blue)
             window.navigationBarColor = ContextCompat.getColor(this, R.color.seed)
         }
-        binding.root.setOnClickListener {
-            auth.signOut()
-        }
+        if (auth.currentUser == null) {
+            val signInLauncher = registerForActivityResult(
+                FirebaseAuthUIActivityResultContract()
+            ) { res ->
+                this.onSignInResult(res)
+            }
 
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            )
+
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+
+            signInLauncher.launch(signInIntent)
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        if (auth.currentUser == null) {
-            launchLoginScreen()
-        }
+
+
     }
+    private fun onSignInResult(res: FirebaseAuthUIAuthenticationResult?) {
+        val response = res?.idpResponse
 
-    private fun launchLoginScreen() {
-        val signInLauncher = registerForActivityResult(
-            FirebaseAuthUIActivityResultContract()
-        ) { res ->
-            this.onSignInResult(res)
-        }
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.AnonymousBuilder().build()
-        )
+        if (res?.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
 
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-    }
+            Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show()
 
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            user = FirebaseAuth.getInstance().currentUser
-            // ...
         } else {
-            launchLoginScreen()
+            Toast.makeText(this, "Não foi possível fazer login. Tente Novamente...", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -96,5 +96,4 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = binding.bottomNavegationView
         bottomNavigationView.setupWithNavController(navController)
     }
-
 }
